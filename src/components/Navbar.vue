d05d100878f39131d7c08311edc995fe733214c6
Brandon Whisnant <brandonwhisnant@Brandons-MacBook-Pro.local>
2019-01-22 22:11:11 -0500

    additional changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,25 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="dark">
-    <b-navbar-nav>
-      <b-nav-item href="#">Home</b-nav-item>
-      <b-nav-item href="#products">Products</b-nav-item>
-      <b-nav-item href="/pricing/">Pricing</b-nav-item>
-      <b-nav-item href="#news">News</b-nav-item>
-      <b-nav-item href="#about">About</b-nav-item>
-      <b-nav-item href="/contact/">Contact</b-nav-item>
-      <b-nav-item href="#" v-if="this.userInfo.role ==='admin' && this.userInfo.loggedIn">Admin Only</b-nav-item>
-      <b-nav-item href="#" v-if="this.userInfo.role =='tester' || this.userInfo.loggedIn">Testing Only</b-nav-item>
-    </b-navbar-nav>
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item right>
-        <b-button v-if="this.userInfo.loggedIn === false" size="sm" class="my-2 my-sm-0" @click="userLogin"><font-awesome-icon icon="sign-in-alt" />&nbsp;&nbsp;Sign In</b-button>
-        <b-button v-else size="sm" class="my-2 my-sm-0" @click="userLogout"><font-awesome-icon icon="sign-out-alt" />&nbsp;&nbsp;Sign Out</b-button>
-      </b-nav-item>
-    </b-navbar-nav>
-  </b-navbar>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <div class="container">
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#products">Products</b-nav-item>
+          <b-nav-item href="/pricing/">Pricing</b-nav-item>
+          <b-nav-item href="#news">News</b-nav-item>
+          <b-nav-item href="#about">About</b-nav-item>
+          <b-nav-item href="/contact/">Contact</b-nav-item>
+          <b-nav-item href="#" v-if="this.userInfo.role ==='admin' && this.userInfo.loggedIn">Admin Only</b-nav-item>
+          <b-nav-item href="#" v-if="this.userInfo.role =='tester' || this.userInfo.loggedIn">Testing Only</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right>
+            <b-button v-if="this.userInfo.loggedIn === false" size="sm" class="account-btn my-2 my-sm-0" @click="userLogin"><font-awesome-icon icon="sign-in-alt" />&nbsp;&nbsp;Sign In</b-button>
+            <b-button v-else size="sm" class="account-btn my-2 my-sm-0" @click="userLogout"><font-awesome-icon icon="sign-out-alt" />&nbsp;&nbsp;Sign Out</b-button>
+          </b-nav-item>
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+
 </template>
 
 <script>
@@ -52,7 +55,29 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+nav {
+  height: 50px;
+}
+
+.nav-item{
+  padding: 10px;
+}
+
+.navbar {
+  padding: 0 !important;
+}
+.container {
+  font-size: 14px;
+}
+
+
+.account-btn {
+    color: #333 !important;
+    background-color: #fff !important;
+    border-color: #ccc !important;
+}
 </style>
 
 
